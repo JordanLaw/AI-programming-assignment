22757b492582fe26cfdaf84f98b070c34931173a
JordanLaw <123150515+JordanLaw@users.noreply.github.com>
2023-03-12 14:32:15 +0800

    Add files via upload

---
+++ AI assignment Q2.py
@@ -0,0 +1,38 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+camW = 640
+camH = 480
+cam.set(3, camW)
+cam.set(4, camH)
+boxW = 80
+boxH = 80
+BoxPosX = 250
+BoxPosY = 200
+BoxPosX2 = BoxPosX + boxW
+BoxPosY2 = BoxPosY + boxH
+movingH = -2
+movingV = 2
+
+while True:
+
+    success, frame = cam.read()
+    frameRec = cv2.rectangle(frame, (BoxPosX, BoxPosY), (BoxPosX2, BoxPosY2), (0, 255, 0), 3)
+
+    if BoxPosX <= 0 or BoxPosX2 >= camW -1:
+        movingV = movingV * (-1)
+    if BoxPosY <= 0 or BoxPosY2 >= camH -1:
+        movingH = movingH * (-1)
+
+    BoxPosX = BoxPosX + movingV
+    BoxPosY = BoxPosY + movingH
+    BoxPosX2 = BoxPosX2 + movingV
+    BoxPosY2 = BoxPosY2 + movingH
+
+    cv2.imshow('Frame', frameRec)
+
+    if cv2.waitKey(20) & 0xff == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
---
+++ Assigment Q3.py
@@ -0,0 +1,30 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+camW = 640
+camH = 480
+cam.set(3, camW)
+cam.set(4, camH)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+faceCascade = cv2.CascadeClassifier('Resource/haarcascade_frontalface_default.xml')
+
+while True:
+
+    success, frame = cam.read()
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frameGray = cv2.cvtColor(frameGray, cv2.COLOR_GRAY2BGR)
+    faces = faceCascade.detectMultiScale(frameGray, 1.1, 10)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), ((x+w), (y+h)), (0, 255, 0), 2)
+        frameGray[y:(y+h), x:(x+w)] = frame[y:(y+h), x:(x+w)]
+
+    cv2.putText(frameGray, 'MBS3523 Assignment 1 - Q4  Name: Law Wing Lam', (10, 30), font, 0.7, (0, 255, 0), 2)
+    cv2.imshow('FrameGray', frameGray)
+
+    if cv2.waitKey(20) & 0xff == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
